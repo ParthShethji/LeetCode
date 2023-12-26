fa306726ecf2fe4be9538cf3a1b13a5cddce3da6
Parth Shethji <parthshethji2693@gmail.com>
2023-12-26 20:36:14 +0530

    Time: 3 ms (99.64%), Space: 41.9 MB (25.27%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,31 +1,26 @@
 class Solution {
-    private static final int MOD = (int) (1e9 + 7); // Constant for modulo
-
+    
+    private int M = 1000000007;
+    
     public int numRollsToTarget(int n, int k, int target) {
-        // Handle invalid input
-        if (n < 0 || k < 1 || target < 1) {
-            throw new IllegalArgumentException("Invalid input values");
-        }
-
-        Integer[][] dp = new Integer[n + 1][target + 1];
-        return helper(n, k, target, dp);
+        return helper(n, k, target, new Integer[n + 1][target + 1]);
     }
-
-    private int helper(int n, int k, int sum, Integer[][] dp) {
+    
+    private int helper(int n, int k, int target, Integer[][] dp) {
         if (n == 0) {
-            return sum == 0 ? 1 : 0; // Base case: no dice left
+            return target == 0 ? 1 : 0;
         }
-
-        if (dp[n][sum] != null) {
-            return dp[n][sum];
+        if (n > target || n * k < target) {
+            return 0;
         }
-
+        if (dp[n][target] != null) {
+            return dp[n][target];
+        }
         int res = 0;
-        for (int i = 1; i <= k && i <= sum; i++) { // Optimized loop
-            res = (res + helper(n - 1, k, sum - i, dp)) % MOD;
+        for (int i = 1; i <= k; i++) {
+            res = (res + helper(n - 1, k, target - i, dp)) % M;
         }
-
-        dp[n][sum] = res;
-        return res;
+        dp[n][target] = res;
+        return dp[n][target];
     }
 }
