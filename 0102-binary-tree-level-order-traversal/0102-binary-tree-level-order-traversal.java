b07da0330556eac304526c2224e580e0508cd1d3
Parth Shethji <parthshethji2693@gmail.com>
2024-01-29 16:30:59 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (69.67%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,40 +1,60 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         Queue<TreeNode> q = new LinkedList<>();
+//         List<List<Integer>> finalAns = new ArrayList<List<Integer>>();
+//         if(root==null){
+//             return finalAns;
+//         }
+//         q.add(root);
+//         while(!q.isEmpty()){
+//             int levels = q.size();
+//             List<Integer> subLevels = new ArrayList<>();
+//             for(int i=0;i<levels;i++){
+//                 if(q.peek().left!=null){
+//                     q.add(q.peek().left);
+//                 }
+//                 if(q.peek().right!=null){
+//                     q.add(q.peek().right);
+//                 }
+//                 subLevels.add(q.remove().val);
+//             }
+//             finalAns.add(subLevels);
+//         }
+//         return finalAns;
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        List<List<Integer>> finalAns = new ArrayList<List<Integer>>();
-        if(root==null){
-            return finalAns;
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        levelOrderHelper(root, result, 0);
+        return result;
+    }
+
+    public void levelOrderHelper(TreeNode root, List<List<Integer>> list, int level) {
+        if(root == null) {
+            return;
         }
-        q.add(root);
-        while(!q.isEmpty()){
-            int levels = q.size();
-            List<Integer> subLevels = new ArrayList<>();
-            for(int i=0;i<levels;i++){
-                if(q.peek().left!=null){
-                    q.add(q.peek().left);
-                }
-                if(q.peek().right!=null){
-                    q.add(q.peek().right);
-                }
-                subLevels.add(q.remove().val);
-            }
-            finalAns.add(subLevels);
+        if(level >= list.size()) {
+            list.add(new LinkedList<Integer>());
         }
-        return finalAns;
+        list.get(level).add(root.val);
+        levelOrderHelper(root.left, list, level+1);
+        levelOrderHelper(root.right, list, level+1);
     }
 }
