19ee41b1ab4e8f5db3e4a969332b29c891e517ad
Parth Shethji <parthshethji2693@gmail.com>
2024-03-16 15:56:16 +0530

    Time: 19 ms (94.44%), Space: 55.3 MB (21.07%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,21 +1,26 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        int left = 0, right = 0, len = nums.length;
-        for(int indx = 0; indx < len; indx++){
-           if(nums[indx] == 0) 
-            nums[indx]  = -1;
-        }
-        int prefSum = 0;
-        Map<Integer, Integer> prefSumIndxMp = new HashMap<>();
-        prefSumIndxMp.put(0, -1);
-        int maxSubLen = 0;
-        for(int indx = 0; indx < len; indx++){
-            prefSum += nums[indx];
-            if(prefSumIndxMp.containsKey(prefSum - 0)){
-                maxSubLen = Math.max(maxSubLen, indx - prefSumIndxMp.get(prefSum - 0));
+        int n=nums.length;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                nums[i]=-1;
             }
-            if(!prefSumIndxMp.containsKey(prefSum)) prefSumIndxMp.put(prefSum, indx);
         }
-        return maxSubLen;
+        int max=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+            if(sum==0){
+                max=Math.max(max,i+1);
+            }
+            if(map.containsKey(sum)){
+                max=Math.max(max,i-map.get(sum));
+            }
+            else{
+                map.put(sum,i);
+            }
+        }
+        return max;
     }
 }
