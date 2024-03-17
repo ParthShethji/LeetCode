a7ebbada9d3ff64e68db962b35d7b02349723601
Parth Shethji <parthshethji2693@gmail.com>
2024-03-17 18:07:07 +0530

    Time: 1 ms (98.87%), Space: 44.6 MB (85.53%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,28 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int i = 0;
-        int n = intervals.length;
         List<int[]> result = new ArrayList<>();
+        int i = 0;
         
-        while (i < intervals.length) {
-            if (intervals[i][1] < newInterval[0])
-                result.add(intervals[i]);
-            else if (intervals[i][0] > newInterval[1]){
-                break;
-            } else {
-                //Overlap : merge them
-                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            }
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
             i++;
         }
         
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
         result.add(newInterval);
-        while (i < n){
-            result.add(intervals[i++]);
+        
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
         }
         
-        return result.toArray(new int[result.size()][2]);
+        int[][] arr = new int[result.size()][2];
+        for (int j = 0; j < result.size(); j++) {
+            arr[j] = result.get(j);
+        }
+        
+        return arr;
     }
 }
-
