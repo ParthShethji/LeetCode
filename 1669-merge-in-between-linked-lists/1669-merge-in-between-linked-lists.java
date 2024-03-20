a9102bdd54dd711393ceae9ce2af0640a85af320
Parth Shethji <parthshethji2693@gmail.com>
2024-03-20 13:27:01 +0530

    Time: 1 ms (100.00%), Space: 47.1 MB (8.41%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -10,33 +10,28 @@
  */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        ListNode prev = list1, temp1 = list1, curr = list1;
-        a--; // Adjust a to match zero-based indexing
-        
-        // Traverse list1 to find the node at position (a-1)
-        while (temp1 != null && a-- > 0) {
-            temp1 = temp1.next;
-            prev = temp1;
+        ListNode first=list1;
+        ListNode second=list1;
+        ListNode ans=first;
+        int i=1;int j=0;
+        while(i<a){
+            first=first.next;
+            i++;
         }
-        
-        temp1 = list1; // Reset temp1 to traverse list1 again
-        
-        // Traverse list1 to find the node at position (b+1)
-        while (temp1 != null && b-- > 0) {
-            temp1 = temp1.next;
-            curr = temp1;
+        while(j<b){
+        second=second.next;j++;
         }
-        
-        ListNode temp2 = list2; // Initialize pointer to traverse list2
-        
-        // Traverse list2 to find the last node
-        while (temp2.next != null) {
-            temp2 = temp2.next;
-        }
-        
-        prev.next = list2; // Connect the node before position 'a' to the head of list2
-        temp2.next = curr.next; // Connect the last node of list2 to the node after position 'b'
-        
-        return list1; // Return the head of list1
+   first.next=list2;
+    ListNode secon=list2;
+while(secon.next!=null){
+    secon=secon.next;
+}
+secon.next=second.next;
+
+
+//System.out.println(first.next.val);
+//System.out.println(second.val);
+
+ return ans;   
     }
 }
