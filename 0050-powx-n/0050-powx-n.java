87269c6a237ef8c1b14342ac8a3ddc9b1d6220bb
Parth Shethji <parthshethji2693@gmail.com>
2024-10-11 14:01:15 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (9.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        double ans =1;
+        if(x==1)    return x;
+        if(n<0){
+            x=1/x;
+            n= -n;
+        }
+        else if(n==0){
+            return 1;
+        }
+        
+        while(n!=0){
+            if((n&1)!=0){
+                ans *= x;
+            }
+            x*=x;
+            n>>>=1;
+        }
+        return ans;
     }
 }
