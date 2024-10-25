ca5cc984f836d3f3670e41e1931c65beba9f2462
Parth Shethji <parthshethji2693@gmail.com>
2024-10-25 14:12:05 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (27.26%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,23 +1,24 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix[0].length-1;
-//         transpose matrix
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(i>j){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i<j){
                     int temp = matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
                 }
             }
         }
         
-//         swap cols from middle
-        for(int i=0;i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length/2; j++){
-                    int temp = matrix[i][j];
-                    matrix[i][j]=matrix[i][n-j];
-                    matrix[i][n-j]=temp;            }
-        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+            matrix[i][j]=matrix[i][n-j-1];
+            matrix[i][n-j-1]=temp;
+            }
+                    }
     }
+    
 }
