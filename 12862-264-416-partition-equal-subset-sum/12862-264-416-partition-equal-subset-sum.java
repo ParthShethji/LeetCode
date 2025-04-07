98854914d50d143f339e02a1e20a1a2f21d9f47a
Parth Shethji <parthshethji2693@gmail.com>
2025-04-07 11:11:52 +0530

    Time: 26 ms, Memory: 42.2 MB - LeetHub

--- 12862-264-416-partition-equal-subset-sum/12862-264-416-partition-equal-subset-sum.java
+++ 12862-264-416-partition-equal-subset-sum/12862-264-416-partition-equal-subset-sum.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int total = 0;
-        for (int i : nums) total += i;
-
-        // If total sum is odd, can't partition
-        if (total % 2 != 0) return false;
+        for (int num : nums) total += num;
         
-        int n = nums.length;
+        if (total % 2 != 0) return false; // If total sum is odd, can't partition
+
         int sum = total / 2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true; // We can always make 0 sum with empty subset
 
-        boolean[][] dp = new boolean[n + 1][sum + 1];
-
-        // Base case: sum = 0 is always possible
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
-        }
-
-        // Filling the DP table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                dp[i][j] = dp[i-1][j]; // not pick
-
-                if (j >= nums[i-1]) {
-                    dp[i][j] = dp[i][j] || dp[i-1][j - nums[i-1]]; // pick
-                }
+        for (int num : nums) {
+            for (int j = sum; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
             }
         }
 
-        return dp[n][sum]; // check at the end
+        return dp[sum];
     }
 }
