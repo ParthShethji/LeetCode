11f6d51709a46f62afcd14c9c4da17bc90e7cd29
Parth Shethji <parthshethji2693@gmail.com>
2026-01-02 18:50:49 +0530

    Time: 0 ms (100%), Space: 28.3 MB (97.6%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n = nums.size();
+                
+        for (int i = 0; i + 1 < n; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+            if (i + 2 < n && nums[i] == nums[i + 2]) {
+                return nums[i];
+            }
+        }
+        
+        return nums[0];
+    }
+};
