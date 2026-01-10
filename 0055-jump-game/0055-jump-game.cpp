a8367b6421a27428d4943d2bf375d9a8a843d3ac
Parth Shethji <parthshethji2693@gmail.com>
2026-01-10 19:36:11 +0530

    Time: 0 ms (100%), Space: 52.3 MB (66.26%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n= nums.size();
+        int lastGood = n-1;
+
+        for(int i = n-1; i>=0; i--){
+            if(i + nums[i]>=lastGood)   lastGood = i;
+        }
+
+        return lastGood == 0;
+    }
+};
