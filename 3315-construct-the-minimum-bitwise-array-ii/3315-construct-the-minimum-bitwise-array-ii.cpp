8549e0607a272b11ed290afb9b6d77e983617ce9
Parth Shethji <parthshethji2693@gmail.com>
2026-01-21 21:52:21 +0530

    Time: 0 ms (100%), Space: 28.2 MB (61.17%) - LeetHub

---
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> res;
+        for (int num: nums) {
+            if (num % 2 == 0) {
+                res.push_back(-1);
+            } else {
+                int lowbit = 0;
+                for (int i = num; i & 1; i >>= 1) {
+                    lowbit = (lowbit << 1) + 1;
+                }
+                int highbit = num - lowbit;
+                res.push_back(highbit | (lowbit >> 1));
+            }
+        }
+        return res;
+    }
+};
