3f0e1cfbbc8fabe3d2740607d846dedd737bd2a9
Parth Shethji <parthshethji2693@gmail.com>
2026-05-07 21:17:20 +0530

    Time: 0 ms (100%), Space: 43.2 MB (15.59%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,9 +14,21 @@
  * }
  */
 class Solution {
+    private boolean helper(TreeNode p, TreeNode q){
+               if(p==null && q == null){
+            return true;
+        }
+        else if(p==null || q==null){
+            return false;
+        }
+        boolean left = p.val == q.val;
+        boolean right = p.val == q.val;
+        boolean leftTree = helper(p.left, q.left);
+        boolean rightTree = helper(p.right, q.right);
+        return (left && right && leftTree && rightTree);    
+    }
+  
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null)  return true;
-        else if(p==null || q==null)     return false;
-        return (p.val==q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        return helper(p,q);
     }
 }
