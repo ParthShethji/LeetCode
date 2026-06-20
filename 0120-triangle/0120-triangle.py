708c489810527411d3f62b523f3b7c4ecc39c511
Parth Shethji <parthshethji2693@gmail.com>
2026-06-21 01:11:41 +0530

    Time: 0 ms (100%), Space: 20 MB (83.55%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,21 +1,9 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        n = len(triangle)
+        dp = triangle[-1][:]
 
-        dp = [[None] * len(row) for row in triangle]
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
 
-        def helper(i, j):
-            if i == n - 1:
-                return triangle[i][j]
-
-            if dp[i][j] is not None:
-                return dp[i][j]
-
-            down = helper(i + 1, j)
-            diagonal = helper(i + 1, j + 1)
-
-            dp[i][j] = triangle[i][j] + min(down, diagonal)
-
-            return dp[i][j]
-
-        return helper(0, 0)
+        return dp[0]
